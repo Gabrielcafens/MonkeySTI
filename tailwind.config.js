22ac745563aa77c958f25b20d4f8ce66f2706d4d
gabrielNETEAM <gabriel.cafe@ufrpe.br>
2024-06-06 16:05:57 -0300

    fix version update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,35 @@
 module.exports = {
-    purge: [
-        "./src/**/*.html",
-        "./src/**/*.jsx"
+    content: [
+      './src/**/*.{js,jsx,ts,tsx,html}'
     ],
-    darkMode: "media", // or 'media' or 'class'
+    darkMode: 'media', // ou 'class'
     theme: {
-        fontFamily: {
-            "sans": ["Dosis"],
+      fontFamily: {
+        sans: ['Dosis']
+      },
+      extend: {
+        boxShadow: {
+          lg: '5px 10px 30px #e5e7eb',
+          xl: '10px 25px 20px #e5e7eb',
+          dark: '5px 10px 30px #1F2937'
         },
-        extend: {
-            boxShadow: {
-                "lg": "5px 10px 30px #e5e7eb",
-                "xl": "10px 25px 20px #e5e7eb",
-                "dark": "5px 10px 30px #1F2937"
-            },
-            fontSize: {
-                "lg": "1.25rem",
-                "xl": "1.4rem",
-                "3xl": "2rem"
-            },
-            width: {
-                "148": "37rem"
-            }
+        fontSize: {
+          lg: '1.25rem',
+          xl: '1.4rem',
+          '3xl': '2rem'
         },
+        width: {
+          '148': '37rem'
+        }
+      }
     },
     variants: {
-        extend: {
-            boxShadow: ["dark"]
-        },
+      extend: {
+        boxShadow: ['dark']
+      }
     },
     plugins: [
-        require("@tailwindcss/forms")
-    ],
-};
+      require('@tailwindcss/forms')
+    ]
+  };
+  
